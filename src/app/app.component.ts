399d6db8cd70f6c0d007ccb4b5e18f050f0d3a92
natasha <dinulya05@yandex.ru>
2023-03-12 15:13:13 +0300

    ngClass

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,26 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  backgroundToggle = 0
+  // backgroundToggle = 0
+  // toggle: any = false
+  // arr = [1, 1, 2, 3, 5, 8, 13]
+  // obj = [
+  //   {
+  //     title: 'Post 1', author: 'Vladilen 1', comments: [
+  //       {name: 'Max 1', text: 'lorem 1'},
+  //       {name: 'Max 1', text: 'lorem 2'},
+  //       {name: 'Max 1', text: 'lorem 3'},
+  //     ]
+  //   },
+  //   {
+  //     title: 'Post 2', author: 'Vladilen 2', comments: [
+  //       {name: 'Max 2', text: 'lorem 1'},
+  //       {name: 'Max 2', text: 'lorem 2'},
+  //       {name: 'Max 2', text: 'lorem 3'},
+  //     ]
+  //   },
+  //
+  // ]
 
+  now: Date = new Date()
 }
