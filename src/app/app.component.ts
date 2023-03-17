1291b5cca02db7742b9c70d8ce4e29360066cd3e
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 <dinulya05@yandex.ru>
2023-03-17 18:52:09 +0300

    forms_validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,76 +1,31 @@
-import {Component} from '@angular/core';
-import {Observable} from "rxjs";
-import {CounterService} from "./services/counter.service";
-import {LocalCounterService} from "./services/local-counter.service";
-
-export interface Post {
-  title: string
-  text: string
-  d: Date
-  id?: number
-
-
-}
-
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-  providers: [LocalCounterService]
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  search = ''
-  searchField = 'title'
-
-  post: Post[] = [
-    {title: 'Milk', text: 'The best milk in the world', d: new Date},
-    {title: 'Bread', text: 'The best bread in the world', d: new Date(2023, 2, 15, 3, 24, 0)},
-    {title: 'Cheese', text: 'The best cheese in the world', d: new Date(2023, 2, 13, 3, 24, 0)}
-  ]
-
-  addPost() {
-    this.post.unshift({
-      title: 'Angular',
-      text: 'Angular pipes',
-      d: new Date()
-
+export class AppComponent implements  OnInit {
+  form: FormGroup
+  submit() {
+    if (this.form.valid) {
+      console.log('Form: ', this!.form)
+      const formData = {...this.form.value}
+      console.log('Form Data: ', formData)
+    }
+  }
+  ngOnInit() {
+    this.form = new FormGroup({
+      email: new FormControl('', [Validators.email, Validators.required]),
+      password: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(6)
+      ])
     })
+
   }
 
-  p: Promise<string> = new Promise<string>(resolve => {
-    setTimeout(() => {
-      resolve('Promise Resolved')
-    }, 4000)
-  })
-  date: Observable<Date> = new Observable(obs => {
-    setInterval(() => {
-      obs.next(new Date())
-    }, 1000)
-  })
-
-  constructor(
-    public counterService: CounterService,
-    public localCounterService: LocalCounterService
-    ) {}
-
-  // updatePost(post: Post) {
-  //   let maxId = 0
-  //   this.post.forEach(value => {
-  //     if (value.id && value.id > maxId) {
-  //       maxId = value.id
-  //     }
-  //   })
-  // }
 }
-
-
-// сделай, чтобы если дата сегодняшнее,
-// то выводило просто время,
-// если вчерашенее, то Yesterday, hh:mm:ss
-// А если позавчера и далее, то полное ГГ:ММ:ДД ЧЧ:мм:сс
-// месяц полностью
-
-// new Date(2023, 2, 13, 3, 24, 0)             2023, 2, 13, 3, 24, 0
 
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import {FormsModule} from "@angular/forms";
-import {StyleDirective} from "./directives/style.directives";
-import { IfNotDirective } from './directives/if-not.directive';
-import { FilterPipe } from './pipes/filter.pipe';
-import { DatePipePipe } from './pipes/date-pipe.pipe';
-import { StrlengthPipe } from './pipes/strlength.pipe';
-import {CounterService} from "./services/counter.service";
-import { CounterComponent } from './counter/counter.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent,
-    StyleDirective,
-    IfNotDirective,
-    FilterPipe,
-    DatePipePipe,
-    StrlengthPipe,
-    CounterComponent
+
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [
-    CounterService
-
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
