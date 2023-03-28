a8944b66d22f46d27bedeca97f41038cd10b0df0
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 <dinulya05@yandex.ru>
2023-03-28 23:30:45 +0300

    unit

---
+++ src/app/counter/counter.component.ts
@@ -0,0 +1,19 @@
+import {Component, EventEmitter, Output} from "@angular/core";
+
+@Component({
+  selector: 'app-counter',
+  template: `Counter: {{counter}}`
+})
+export class CounterComponent {
+  counter = 0
+  @Output() counterEmitter = new EventEmitter<number>()
+
+  increment() {
+    this.counter++
+    this.counterEmitter.emit(this.counter)
+  }
+
+  decrement() {
+    this.counter--
+  }
+}
