a585ba2da442e8ddf3a7312ce6ea8f3f99578750
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 <dinulya05@yandex.ru>
2023-03-29 19:42:06 +0300

    unit

--- src/app/posts/posts.component.spec.ts
+++ src/app/posts/posts.component.spec.ts
@@ -2,6 +2,7 @@ import {PostsComponent} from "./posts.component";
 import {PostsService} from "./posts.service";
 import {EMPTY, of, throwError} from "rxjs";
 
+
 describe('PostsComponent', () => {
   let component: PostsComponent
   let service: PostsService
