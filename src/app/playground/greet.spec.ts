1f89de8386e2f16bb0123c5288e09838e205d82b
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 <dinulya05@yandex.ru>
2023-03-29 23:42:21 +0300

    integration test

--- src/app/playground/greet.spec.ts
+++
@@ -1,8 +0,0 @@
-import {greet} from "./greet";
-
-describe('greet', () => {
-  it('should include name in return message', function () {
-    expect(greet('Angular')).toContain('Angular')
-
-  });
-})
