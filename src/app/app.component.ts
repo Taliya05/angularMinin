d8b3ccfe7f0afffee491930a0c80e65bcbc8f694
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 <dinulya05@yandex.ru>
2023-03-30 23:14:57 +0300

    animate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,29 @@
 import {Component} from '@angular/core';
+import {state, style, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('box', [
+      state('start', style({background: 'blue'})),
+      state('end', style({
+        background: 'red',
+        transform: 'scale(1.2)'
+      }))
+    ])
+  ]
 })
 export class AppComponent {
+  boxState = 'end'
 
   constructor() {
   }
 
+  animate() {
+    this.boxState = this.boxState === 'end' ? 'start' : 'end'
+  }
 }
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,17 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
 import {FormsModule} from "@angular/forms";
-import {CounterComponent} from "./counter/counter.component";
-import {PostsComponent} from "./posts/posts.component";
-import {RoutingComponent} from './routing/routing.component';
-import {RouterModule} from "@angular/router";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CounterComponent,
-    PostsComponent,
-    RoutingComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot([])
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
