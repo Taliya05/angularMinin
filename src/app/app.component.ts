d74864dd73a5147e79cd25b49e4acd7346bb4242
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 <dinulya05@yandex.ru>
2023-03-31 12:25:19 +0300

    animate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {state, style, trigger} from "@angular/animations";
+import {animate, state, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-root',
@@ -11,12 +11,21 @@ import {state, style, trigger} from "@angular/animations";
       state('end', style({
         background: 'red',
         transform: 'scale(1.2)'
-      }))
+      })),
+      state('special', style(({
+        background: 'orange',
+        transform: 'scale(0.5)',
+        borderRadius: '50%'
+      }))),
+      transition('start => end', animate(450)),
+      transition('end => start', animate('800ms ease-in-out')),
+      transition('* <=> special', [style({background: 'green'}), animate('1s', style({background: 'pink'})), animate(750)]),
+
     ])
   ]
 })
 export class AppComponent {
-  boxState = 'end'
+  boxState = 'start'
 
   constructor() {
   }
